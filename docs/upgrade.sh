ffaa36d6a239e7d752a28b24a4881e8169884c84
Matt S.Y. Ho <methodho@gmail.com>
2018-06-06 11:39:43 +0800

    update

--- docs/upgrade.sh
+++ docs/upgrade.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-docker pull softleader/captain-kube
-# docker pull hub.softleader.com.tw/captain-kube
-docker-compose down
-docker-compose up -d
+docker pull softleader/captain-kube | grep -q 'Image is up to date'
+# docker pull hub.softleader.com.tw/captain-kube | grep -q 'Image is up to date'
+
+if [ $? -eq 0 ]; then
+    echo "Image already up to date"
+else
+    docker-compose down
+    docker-compose up -d
+fi
 
 exit 0

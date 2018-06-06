65919d7c89d9efe033402ee17a89ed5b748579fd
Matt S.Y. Ho <methodho@gmail.com>
2018-06-06 11:41:36 +0800

    update

--- publish.sh
+++ publish.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 make
-docker build -t hub.softleader.com.tw/captain-kube .
-docker push hub.softleader.com.tw/captain-kube
-docker tag hub.softleader.com.tw/captain-kube softleader/captain-kube
-docker push softleader/captain-kube
+
+if [ $? -eq 0 ]; then
+    docker build -t hub.softleader.com.tw/captain-kube .
+    docker push hub.softleader.com.tw/captain-kube
+    docker tag hub.softleader.com.tw/captain-kube softleader/captain-kube
+    docker push softleader/captain-kube
+fi

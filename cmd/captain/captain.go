ba9eb5fccc9e35d08d3ca54ca5fcbc6bccca06c7
matt <methodho@gmail.com>
2018-12-18 11:51:29 +0800

    update

--- cmd/captain/captain.go
+++ cmd/captain/captain.go
@@ -19,8 +19,8 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 	r, err := c.Pull(ctx, &image.PullRequest{
-		Host: "softleader",
-		Repo: "caplet",
+		Host:  "softleader",
+		Repo:  "caplet",
 	})
 	if err != nil {
 		log.Fatalf("could not pull image: %v", err)

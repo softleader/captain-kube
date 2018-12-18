8a1cae54ff5e94db0f8de707faeb1d241d875487
sevenlfanks <godjdc@gmail.com>
2018-12-18 16:33:55 +0800

    fileupload demo

---
+++ cmd/filedemo/client.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"github.com/softleader/captain-kube/pkg/file"
+	"google.golang.org/grpc"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:50052", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	c := file.NewUploaderClient(conn)
+	ctx := context.Background()
+
+	f, err := os.Open("./data/APPLICATION_2.pdf")
+	if err != nil {
+		log.Fatalf("read file failed: %v", err)
+	}
+
+	stream, err := c.Upload(ctx)
+	if err != nil {
+		log.Fatalf("open upload stream failed: %v", err)
+	}
+
+	buf := make([]byte, 4)
+	for {
+		if n, err := f.Read(buf); err == nil {
+			stream.Send(&file.Chunk{
+				Content: buf[:n],
+			})
+		} else {
+			if err == io.EOF {
+				break
+			} else {
+				log.Fatalf("uploading stream failed: %v", err)
+			}
+		}
+	}
+	stream.CloseAndRecv()
+
+	log.Printf("file uploaded")
+}
---
+++ cmd/filedemo/server.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"github.com/softleader/captain-kube/pkg/file"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+const (
+	port = ":50052"
+)
+
+type server struct{}
+
+func (s *server) Upload(stream file.Uploader_UploadServer) error {
+	log.Println("reciving file")
+
+	os.Remove("./data/uploaded.txt")
+	f, err := os.Create("./data/uploaded.txt")
+	if err != nil {
+		return fmt.Errorf("failed to create file: %s", err)
+	}
+
+	for {
+		c, err := stream.Recv()
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				return fmt.Errorf("failed unexpectadely while reading chunks from stream: %s", err)
+			}
+		}
+		b := c.GetContent()
+		log.Println("reciving", len(b), "byte")
+		f.Write(b)
+	}
+
+	info, _ := f.Stat()
+	log.Println("file recived: ", info.Size(), "bytes")
+	defer f.Close()
+
+	return stream.SendAndClose(&file.UploadResponse{
+		Message: "Upload received with success",
+	})
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	file.RegisterUploaderServer(s, &server{})
+
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+
+}

07d7a6a24bcb4208d20a88391e0eb7b63167c872
matt <methodho@gmail.com>
2018-12-19 17:51:27 +0800

    add flag

--- cmd/captain/app/server/captain_install_chart.go
+++ cmd/captain/app/server/captain_install_chart.go
@@ -19,25 +19,31 @@ func (s *CaptainServer) InstallChart(c context.Context, req *proto.InstallChartR
 	}
 	chartFile := "" // TODO
 	chartPath := filepath.Join(path, req.GetChart().GetFileName())
-	if err = arc.Extract(s.out, chartFile, chartPath); err != nil {
-		return
-	}
-	tplPath := filepath.Join(chartPath, template)
-	if err := helm.Template(s.out, chartPath, tplPath); err != nil {
-		return
-	}
-	images, err := chart.CollectImages(tplPath)
-	if err != nil {
-		return nil, err
-	}
 
-	caplet.PullImage(s.out, s.endpoints, s.port, newPullImageRequest(images), req.GetTimeout())
+	if req.GetSync() {
+		if err = arc.Extract(s.out, chartFile, chartPath); err != nil {
+			return
+		}
+		tplPath := filepath.Join(chartPath, template)
+		if err := helm.Template(s.out, chartPath, tplPath); err != nil {
+			return
+		}
+		images, err := chart.CollectImages(tplPath)
+		if err != nil {
+			return nil, err
+		}
+		caplet.PullImage(s.out, s.endpoints, s.port, newPullImageRequest(images), req.GetTimeout())
+	}
 
 	// TODO: how to get caplet out?
 	resp = &proto.InstallChartResponse{
 		Out: "looks good!!?",
 	}
 	return
+}
+
+func sync() {
+
 }
 
 func newPullImageRequest(images chart.Images) (req *proto.PullImageRequest) {

5fa0e70e251b0603f2d160c751df77578f60bcd2
matt <methodho@gmail.com>
2018-12-20 17:56:04 +0800

    update

--- cmd/captain/app/server/captain_generate_script.go
+++ cmd/captain/app/server/captain_generate_script.go
@@ -2,17 +2,16 @@ package server
 
 import (
 	"bytes"
-	"context"
 	"github.com/softleader/captain-kube/pkg/helm/chart"
 	"github.com/softleader/captain-kube/pkg/proto"
 	"strings"
 )
 
-func (s *CaptainServer) GenerateScript(c context.Context, req *proto.GenerateScriptRequest) (resp *proto.GenerateScriptResponse, err error) {
+func (s *CaptainServer) GenerateScript(req *proto.GenerateScriptRequest, stream proto.Captain_GenerateScriptServer) error {
 	chartFile := req.GetChart().GetFileName() // TODO
 	tpls, err := chart.LoadArchive(s.out, chartFile)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	var buf bytes.Buffer
@@ -20,38 +19,50 @@ func (s *CaptainServer) GenerateScript(c context.Context, req *proto.GenerateScr
 	if from, to := strings.TrimSpace(req.GetRetag().GetFrom()), strings.TrimSpace(req.GetRetag().GetTo()); from != "" && to != "" {
 		script, err := tpls.GenerateReTagScript(from, to)
 		if err != nil {
-			return nil, err
+			return err
 		}
-		buf.Write(script.Bytes())
+		if err := stream.Send(&proto.GenerateScriptResponse{
+			Out: script.Bytes(),
+		}); err != nil {
+			return err
+		}
 	}
 
 	if req.Pull {
 		script, err := tpls.GeneratePullScript()
 		if err != nil {
-			return nil, err
+			return err
 		}
-		buf.Write(script.Bytes())
+		if err := stream.Send(&proto.GenerateScriptResponse{
+			Out: script.Bytes(),
+		}); err != nil {
+			return err
+		}
 	}
 
 	if req.Load {
 		script, err := tpls.GenerateLoadScript()
 		if err != nil {
-			return nil, err
+			return err
 		}
-		buf.Write(script.Bytes())
+		if err := stream.Send(&proto.GenerateScriptResponse{
+			Out: script.Bytes(),
+		}); err != nil {
+			return err
+		}
 	}
 
 	if req.Save {
 		script, err := tpls.GenerateSaveScript()
 		if err != nil {
-			return nil, err
+			return err
 		}
-		buf.Write(script.Bytes())
+		if err := stream.Send(&proto.GenerateScriptResponse{
+			Out: script.Bytes(),
+		}); err != nil {
+			return err
+		}
 	}
 
-	resp = &proto.GenerateScriptResponse{
-		Out: buf.String(),
-	}
-
-	return
+	return nil
 }

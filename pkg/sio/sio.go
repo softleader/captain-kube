37cc9f381b1c7e398dc50ff40bf2929c39e599cb
matt <methodho@gmail.com>
2018-12-21 10:30:44 +0800

    update

--- pkg/sio/sio.go
+++ pkg/sio/sio.go
@@ -1,15 +1,15 @@
 package sio // stand for streaming io
 
 type StreamWriter struct {
-	sender func(p []byte) error
+	send func(p []byte) error
 }
 
 func (s *StreamWriter) Write(p []byte) (n int, err error) {
 	n = len(p)
-	err = s.sender(p)
+	err = s.send(p)
 	return
 }
 
-func NewStreamWriter(sender func(p []byte) error) *StreamWriter {
-	return &StreamWriter{sender}
+func NewStreamWriter(send func(p []byte) error) *StreamWriter {
+	return &StreamWriter{send}
 }

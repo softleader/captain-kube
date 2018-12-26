6e18b572c181b293291fd6410024273bec7ffca4
matt <methodho@gmail.com>
2018-12-26 23:20:54 +0800

    add usage

--- cmd/capctl/app/ctx.go
+++ cmd/capctl/app/ctx.go
@@ -12,6 +12,8 @@ const ctxHelp = `Switch between Captain-Kubes back and forth
 	ctx -                   : switch to the previous context
 	ctx rm <NAME>           : delete context <NAME> ('.' for current-context)
 	ctx add <NAME> <ARGS..> : add context <NAME> with <ARGS...>
+
+	參數的讀取順序為: 當前 flags > ctx > os.Lookup
 `
 
 type ctxCmd struct {
@@ -36,6 +38,7 @@ func newCtxCmd() *cobra.Command {
 
 func (c *ctxCmd) run() error {
 
+	// TODO:
 	// os.Lookup("SL_PLUGIN_MOUNT") 可以取得從 slctl 傳入的 plugin 資料儲存位置
 	// 如果沒有發現 $SL_PLUGIN_MOUNT 這個 command 應該就要中斷
 	// 反之 ctx 就可以儲存在那 $SL_PLUGIN_MOUNT 目錄下

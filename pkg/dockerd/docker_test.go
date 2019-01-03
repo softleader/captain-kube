b7ab27cfdd94eddf1e3edc4b38c163f659539620
matt <methodho@gmail.com>
2019-01-03 15:05:14 +0800

    update

--- pkg/dockerd/docker_test.go
+++ pkg/dockerd/docker_test.go
@@ -1,8 +1,6 @@
 package dockerd
 
 import (
-	"github.com/sirupsen/logrus"
-	"github.com/softleader/captain-kube/pkg/helm/chart"
 	"github.com/softleader/captain-kube/pkg/proto"
 	"testing"
 )
@@ -23,29 +21,5 @@ func TestEncode(t *testing.T) {
 	expected := "eyJVc2VybmFtZSI6ImEiLCJQYXNzd29yZCI6ImIifQ=="
 	if encoded != expected {
 		t.Errorf("expected %q, but get %q", expected, encoded)
-	}
-}
-
-func TestPull(t *testing.T) {
-	if err := Pull(logrus.StandardLogger(), chart.Image{
-		Host: "library",
-		Repo: "ubuntu",
-		Tag:  "xenial",
-	}, &proto.RegistryAuth{
-		Username: "dev",
-		Password: "sleader",
-	}); err != nil {
-		t.Error(err)
-	}
-
-	if err := Pull(logrus.StandardLogger(), chart.Image{
-		Host: "hub.softleader.com.tw",
-		Repo: "softleader-common-mail-rpc",
-		Tag:  "latest",
-	}, &proto.RegistryAuth{
-		Username: "dev",
-		Password: "sleader",
-	}); err != nil {
-		t.Error(err)
 	}
 }

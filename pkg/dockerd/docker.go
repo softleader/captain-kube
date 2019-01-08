3ee2073c3ae7a4eb8e9979123bc68f24ee3fd681
matt <methodho@gmail.com>
2019-01-08 10:28:09 +0800

    update

--- pkg/dockerd/docker.go
+++ pkg/dockerd/docker.go
@@ -18,9 +18,10 @@ func encode(ra *proto.RegistryAuth) (string, error) {
 func isDockerUnauthorized(err error) bool {
 	if err != nil {
 		if _, ok := err.(*docker.Error); ok {
-			// TODO 判斷是 no basic auth credentials
+			// 從 header 的文字判斷是不好的做法, 畢竟 docker daemon 升級後也許會調整
+			// 因此這邊先當成只要是 docker 回復的錯誤就是 unauthorized
+			// return strings.Contains(derr.Message, "missing X-Registry-Auth")
 			return true
-			// return derr.Status == 500 && strings.HasSuffix(derr.Message, "no basic auth credentials")
 		}
 	}
 	return false
--- pkg/dockerd/docker_test.go
+++ pkg/dockerd/docker_test.go
@@ -1,7 +1,10 @@
 package dockerd
 
 import (
+	"context"
+	"github.com/fsouza/go-dockerclient"
 	"github.com/softleader/captain-kube/pkg/proto"
+	"github.com/softleader/captain-kube/pkg/utils/tcp"
 	"testing"
 )
 
@@ -21,5 +24,25 @@ func TestEncode(t *testing.T) {
 	expected := "eyJVc2VybmFtZSI6ImEiLCJQYXNzd29yZCI6ImIifQ=="
 	if encoded != expected {
 		t.Errorf("expected %q, but get %q", expected, encoded)
+	}
+}
+
+func TestIsDockerUnauthorized(t *testing.T) {
+	if !tcp.IsReachable("hub.softleader.com.tw", 8443, 3) {
+		t.Skipf("hub.softleader.com.tw:8443 not reachable")
+	}
+	cli, err := docker.NewClientFromEnv()
+	if err != nil {
+		t.Skipf("can not get docker client")
+	}
+	options := docker.PushImageOptions{
+		Context: context.Background(),
+		Name:    "hub.softleader.com.tw",
+		Tag:     "busybox",
+	}
+
+	err = cli.PushImage(options, docker.AuthConfiguration{})
+	if !isDockerUnauthorized(err) {
+		t.Errorf("err should be unauthorized, but got %s", err)
 	}
 }

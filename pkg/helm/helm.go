88e372a32b5b53c95829884dfe7cd903f91d72f1
matt <methodho@gmail.com>
2019-02-13 14:12:22 +0800

    print err in non-verbose mode

--- pkg/helm/helm.go
+++ pkg/helm/helm.go
@@ -20,8 +20,8 @@ func Template(log *logrus.Logger, chart, outputDir string, set ...string) (err e
 	if log.IsLevelEnabled(logrus.DebugLevel) {
 		log.Out.Write([]byte(fmt.Sprintln(strings.Join(cmd.Args, " "))))
 		cmd.Stdout = log.Out
-		cmd.Stderr = log.Out
 	}
+	cmd.Stderr = log.Out
 	if err := cmd.Start(); err != nil {
 		return err
 	}

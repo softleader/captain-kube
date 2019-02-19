58ae280c7f3eb36cb3b0fdd477eb375f94b1d905
matt <methodho@gmail.com>
2019-02-19 22:31:18 +0800

    update

--- pkg/dur/dur.go
+++ pkg/dur/dur.go
@@ -1,10 +1,16 @@
 package dur
 
-import "time"
+import (
+	"strconv"
+	"time"
+)
 
 var DefaultDeadline = "30m"
 
 func Parse(s string) time.Duration {
+	if i, err := strconv.Atoi(s); err == nil {
+		return time.Duration(i) * time.Second
+	}
 	d, err := time.ParseDuration(s)
 	if err != nil {
 		d, _ = time.ParseDuration(DefaultDeadline)
---
+++ pkg/dur/dur_test.go
@@ -0,0 +1,10 @@
+package dur
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestParse(t *testing.T) {
+	fmt.Println(Parse("5").String())
+}
